e59210274d6817ed5f642b3b7af8a3bb42583077
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2023-01-17 15:26:38 +0100

    Fixed castling being reporting wrongly in the PV
    Bench: 6369857

--- search_mgr.h
+++ search_mgr.h
@@ -106,6 +106,8 @@ namespace Zagreus {
         void getWhiteKnightScore(EvalContext &context, Bitboard &bitboard);
 
         void getBlackKnightScore(EvalContext &context, Bitboard &bitboard);
+
+        std::string getMoveNotation(Bitboard &board, Move &move);
     };
 
     static SearchManager searchManager{};
