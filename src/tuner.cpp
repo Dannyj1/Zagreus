f01e1550a5bcef304b3279ef21db8b12394c52e8
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2023-06-28 14:32:45 +0200

    New tuner params
    Bench: 9475262

--- src/tuner.cpp
+++ src/tuner.cpp
@@ -40,11 +40,11 @@ namespace Zagreus {
     int iteration = 0;
     double K = 0.0;
 
-    int batchSize = 64;
+    int batchSize = 256;
     double learningRate = 0.1;
-    double epsilon = 10.0;
+    double epsilon = 1.0;
     double optimizerEpsilon = 1e-6;
-    double epsilonDecay = 0.97;
+    double epsilonDecay = 0.98;
     double beta1 = 0.9;
     double beta2 = 0.999;
     int epsilonWarmupIterations = 0;
