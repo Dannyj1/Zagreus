a55611472f224ad804d563be90ef2f642c9b3783
Danny Jelsma <73849717+Dannyj1@users.noreply.github.com>
2024-03-24 14:47:14 +0100

    Allow LMR in check
    Bench: 5229888

--- src/search.cpp
+++ src/search.cpp
@@ -239,22 +239,20 @@ int search(Bitboard& board, int alpha, int beta, int16_t depth,
 
             // LMR (Not in PV/Root nodes)
             if (depth >= 3 && !extension && move.captureScore == NO_CAPTURE_SCORE && move.
-                promotionPiece == EMPTY && movePicker.movesSearched() > 4) {
-                if (!board.isKingInCheck<color>() && !board.isKingInCheck<OPPOSITE_COLOR>()) {
-                    int R = 1;
+                promotionPiece == EMPTY && movePicker.movesSearched() >= 5) {
+                int R = 1;
 
-                    // After 60% of the moves have been made, increase R by 1
-                    if (movePicker.movesSearched() > std::ceil(moves->size * 0.6)) {
-                        R += 1;
-                    }
+                // After 60% of the moves have been made, increase R by 1
+                if (movePicker.movesSearched() > std::ceil(moves->size * 0.6)) {
+                    R += 1;
+                }
 
-                    score = -search<OPPOSITE_COLOR, NO_PV>(
-                        board, -alpha - 1, -alpha, depth - 1 - R, context, searchStats, nodeLine);
-                    didLmr = true;
+                score = -search<OPPOSITE_COLOR, NO_PV>(
+                    board, -alpha - 1, -alpha, depth - 1 - R, context, searchStats, nodeLine);
+                didLmr = true;
 
-                    if (score > alpha) {
-                        shouldFullSearch = true;
-                    }
+                if (score > alpha) {
+                    shouldFullSearch = true;
                 }
             }
 
