f275c50e0d6608ebb3d2aef4d22d3263b391e50f
Danny Jelsma <73849717+Dannyj1@users.noreply.github.com>
2024-10-05 17:36:41 +0200

    Implement bishop move generation, fix moves being generated that would capture the opponent king

--- src/move_gen.cpp
+++ src/move_gen.cpp
@@ -35,9 +35,13 @@ void generatePawnMoves(const Board& board, MoveList& moves) {
     constexpr PieceColor opponentColor = color == PieceColor::WHITE
                                              ? PieceColor::BLACK
                                              : PieceColor::WHITE;
+    constexpr Piece opponentKing = opponentColor == PieceColor::WHITE
+                                       ? Piece::WHITE_KING
+                                       : Piece::BLACK_KING;
 
     const uint64_t pawnBB = board.getBitboard<pawn>();
     const uint64_t emptyBB = board.getEmptyBitboard();
+    const uint64_t opponentKingBB = board.getBitboard<opponentKing>();
     const uint64_t opponentPieces = board.getColorBitboard<opponentColor>();
     uint64_t pawnSinglePushes;
     uint64_t pawnDoublePushes;
@@ -56,8 +60,9 @@ void generatePawnMoves(const Board& board, MoveList& moves) {
         pawnEastAttacks = blackPawnEastAttacks(pawnBB);
     }
 
-    pawnWestAttacks &= opponentPieces;
-    pawnEastAttacks &= opponentPieces;
+    pawnWestAttacks &= (opponentPieces & ~opponentKingBB);
+    pawnEastAttacks &= (opponentPieces & ~opponentKingBB);
+
     constexpr Direction fromPushDirection = color == PieceColor::WHITE
                                                 ? Direction::SOUTH
                                                 : Direction::NORTH;
@@ -69,8 +74,8 @@ void generatePawnMoves(const Board& board, MoveList& moves) {
                                                         : Direction::NORTH_EAST;
 
     while (pawnSinglePushes) {
-        const uint64_t squareTo = popLsb(pawnSinglePushes);
-        const uint64_t squareFrom = shift<fromPushDirection>(squareTo);
+        const uint8_t squareTo = popLsb(pawnSinglePushes);
+        const uint8_t squareFrom = shift<fromPushDirection>(squareTo);
         const Move move = encodeMove(squareFrom, squareTo);
 
         moves.list[moves.size] = move;
@@ -78,8 +83,8 @@ void generatePawnMoves(const Board& board, MoveList& moves) {
     }
 
     while (pawnDoublePushes) {
-        const uint64_t squareTo = popLsb(pawnDoublePushes);
-        const uint64_t squareFrom = shift<fromPushDirection>(squareTo);
+        const uint8_t squareTo = popLsb(pawnDoublePushes);
+        const uint8_t squareFrom = shift<fromPushDirection>(squareTo);
         const Move move = encodeMove(squareFrom, squareTo);
 
         moves.list[moves.size] = move;
@@ -87,8 +92,8 @@ void generatePawnMoves(const Board& board, MoveList& moves) {
     }
 
     while (pawnWestAttacks) {
-        const uint64_t squareTo = popLsb(pawnWestAttacks);
-        const uint64_t squareFrom = shift<fromSqWestAttackDirection>(squareTo);
+        const uint8_t squareTo = popLsb(pawnWestAttacks);
+        const uint8_t squareFrom = shift<fromSqWestAttackDirection>(squareTo);
         const Move move = encodeMove(squareFrom, squareTo);
 
         moves.list[moves.size] = move;
@@ -96,8 +101,8 @@ void generatePawnMoves(const Board& board, MoveList& moves) {
     }
 
     while (pawnEastAttacks) {
-        const uint64_t squareTo = popLsb(pawnEastAttacks);
-        const uint64_t squareFrom = shift<fromSqEastAttackDirection>(squareTo);
+        const uint8_t squareTo = popLsb(pawnEastAttacks);
+        const uint8_t squareFrom = shift<fromSqEastAttackDirection>(squareTo);
         const Move move = encodeMove(squareFrom, squareTo);
 
         moves.list[moves.size] = move;
@@ -109,15 +114,43 @@ template <PieceColor color, GenerationType type>
 void generateKnightMoves(const Board& board, MoveList& moves) {
     // TODO: Move to table calculations
     constexpr Piece knight = color == PieceColor::WHITE ? Piece::WHITE_KNIGHT : Piece::BLACK_KNIGHT;
+    constexpr Piece opponentKing = color == PieceColor::WHITE ? Piece::WHITE_KING : Piece::BLACK_KING;
     const uint64_t ownPieces = board.getColorBitboard<color>();
+    const uint64_t opponentKingBB = board.getBitboard<opponentKing>();
     uint64_t knightBB = board.getBitboard<knight>();
 
     while (knightBB) {
-        const uint64_t fromSquare = popLsb(knightBB);
-        uint64_t genBB = knightAttacks(fromSquare) & ~ownPieces;
+        const uint8_t fromSquare = popLsb(knightBB);
+        uint64_t genBB = knightAttacks(fromSquare) & ~ownPieces & ~opponentKingBB;
 
         while (genBB) {
-            const uint64_t toSquare = popLsb(genBB);
+            const uint8_t toSquare = popLsb(genBB);
+            const Move move = encodeMove(fromSquare, toSquare);
+
+            moves.list[moves.size] = move;
+            moves.size++;
+        }
+    }
+}
+
+template <PieceColor color, GenerationType type>
+void generateBishopMoves(const Board& board, MoveList& moves) {
+    using enum Piece;
+    constexpr Piece bishop = color == PieceColor::WHITE ? WHITE_BISHOP : BLACK_BISHOP;
+    constexpr Piece opponentKing = color == PieceColor::WHITE
+                                       ? WHITE_KING
+                                       : BLACK_KING;
+    const uint64_t ownPieces = board.getColorBitboard<color>();
+    const uint64_t opponentKingBB = board.getBitboard<opponentKing>();
+    uint64_t bishopBB = board.getBitboard<bishop>();
+    uint64_t occupied = board.getOccupiedBitboard();
+
+    while (bishopBB) {
+        const uint8_t fromSquare = popLsb(bishopBB);
+        uint64_t genBB = bishopAttacks(fromSquare, occupied) & ~ownPieces & ~opponentKingBB;
+
+        while (genBB) {
+            const uint8_t toSquare = popLsb(genBB);
             const Move move = encodeMove(fromSquare, toSquare);
 
             moves.list[moves.size] = move;
