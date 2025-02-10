59c84e654c06275a973db8ab554d9cf6cbf50b50
Danny Jelsma <73849717+Dannyj1@users.noreply.github.com>
2025-02-10 12:20:53 +0100

    More dynamic R in NMP
    Bench: 8842696

--- src/search.cpp
+++ src/search.cpp
@@ -171,9 +171,9 @@ int pvSearch(Engine& engine, Board& board, int alpha, int beta, int depth, Searc
         // Null Move Pruning
         if (depth >= 3 && !isInCheck && board.hasNonPawnMaterial<color>() && !board.getPreviousMove() == NO_MOVE) {
             board.makeNullMove();
-            const int R = depth >= 6 ? 3 : 2;
+            const int R = (depth / 3) + 1;
             PvLine nmpPvLine = PvLine{board.getPly()};
-            const int nullMoveScore = -pvSearch<opponentColor, REGULAR>(engine, board, -beta, -beta + 1, depth - R - 1,
+            const int nullMoveScore = -pvSearch<opponentColor, REGULAR>(engine, board, -beta, -beta + 1, depth - R,
                                                                         stats, endTime, nmpPvLine);
             board.unmakeNullMove();
 
