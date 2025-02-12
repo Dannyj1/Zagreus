755a0f5534e5461a60618da20d85790e1d324037
Danny Jelsma <73849717+Dannyj1@users.noreply.github.com>
2025-02-12 15:52:06 +0100

    Revert "Don't consider underpromotions in qSearch"
    This reverts commit f89c2761a4cba4fbde29844aad7ecec095163542.

--- src/main.cpp
+++ src/main.cpp
@@ -137,7 +137,7 @@ void benchmark(bool fast) {
     std::vector<std::string> positions = fast ? FAST_BENCHMARK_POSITIONS : BENCHMARK_POSITIONS;
     SearchParams params{};
 
-    params.depth = fast ? 4 : 7;
+    params.depth = fast ? 3 : 6;
 
     for (const std::string& position : positions) {
         for (int i = 0; i < 2; i++) {
