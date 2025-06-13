67f3f3c026e7c7b6caaa181e6cbae1f676084abc
Danny Jelsma <73849717+Dannyj1@users.noreply.github.com>
2025-06-13 14:50:56 +0200

    Revert "Increase backward pawn penalty"
    This reverts commit a42a5e0e826629b93e0fcb7da6d25ce6f62e4cb7.

--- src/eval_features.cpp
+++ src/eval_features.cpp
@@ -42,8 +42,8 @@ int evalPieceOnWeakSquarePenalty[GAME_PHASES]{-4, -1};
 int evalUnoccupiedStrongSquare[GAME_PHASES]{2, 0};
 
 // Pawn structure
-int evalBackwardPawnPenalty[GAME_PHASES] = {-10, -20};
-int evalBackwardPawnOnHalfOpenFilePenalty[GAME_PHASES] = {-10, -20};
+int evalBackwardPawnPenalty[GAME_PHASES] = {-8, -12};
+int evalBackwardPawnOnHalfOpenFilePenalty[GAME_PHASES] = {-8, -12};
 
 int evalBishopPairBonus[GAME_PHASES]{50, 25};
 } // namespace Zagreus
