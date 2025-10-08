05d6e59cc0363e15b582cee0c38face61760e5c1
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2025-10-08 12:53:07 +0000

    Deploying to gh-pages from @ Dannyj1/Zagreus@d27ee22bff33c0852101356dc73ed4bd673b226a 🚀

--- search/all_15.js
+++ search/all_15.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['validationhash_0',['validationHash',['../structZagreus_1_1TTEntry.html#af699059bdb42f1aadfc82f8439bdcba9',1,'Zagreus::TTEntry']]],
+  ['validationhash_0',['validationHash',['../structZagreus_1_1TTEntry.html#a453556dc88a4404d2d41c083875c9ee4',1,'Zagreus::TTEntry']]],
   ['value_1',['value',['../structpcg__extras_1_1static__arbitrary__seed.html#a0168de8c2e22973e0d7802ed69050d6f',1,'pcg_extras::static_arbitrary_seed']]]
 ];
--- search/variables_c.js
+++ search/variables_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['previousmove_0',['previousMove',['../structZagreus_1_1BoardState.html#a7cf2b39201313f5b83883386d50bc246',1,'Zagreus::BoardState']]],
-  ['pvline_1',['pvLine',['../structZagreus_1_1SearchStats.html#a49bc7934f684502191e4f40c2fa1a062',1,'Zagreus::SearchStats']]]
+  ['padding_0',['padding',['../structZagreus_1_1TTEntry.html#af315947cc3f660e6bd5e4613491ced0e',1,'Zagreus::TTEntry']]],
+  ['previousmove_1',['previousMove',['../structZagreus_1_1BoardState.html#a7cf2b39201313f5b83883386d50bc246',1,'Zagreus::BoardState']]],
+  ['pvline_2',['pvLine',['../structZagreus_1_1SearchStats.html#a49bc7934f684502191e4f40c2fa1a062',1,'Zagreus::SearchStats']]]
 ];
