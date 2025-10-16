ffb400be5156cc08c5b692b4a8261ca72524a11f
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2025-10-16 12:48:11 +0000

    Deploying to gh-pages from @ Dannyj1/Zagreus@6fa1d39d4a3a9efd1a42edd50c81cb01ecbfbdb2 🚀

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['addoption_0',['addOption',['../classZagreus_1_1Engine.html#a88a7ddc125e98976af68011c08213a5a',1,'Zagreus::Engine']]],
-  ['addvar_1',['addVar',['../classZagreus_1_1UCIOption.html#a9bd71248e84bb86ff3dc177f5fba4c1a',1,'Zagreus::UCIOption']]],
-  ['advance_2',['advance',['../classpcg__detail_1_1engine.html#a12d3653cfe490a59f7ce55d028f1a7e0',1,'pcg_detail::engine::advance(itype state, itype delta, itype cur_mult, itype cur_plus)'],['../classpcg__detail_1_1engine.html#ad825cdb944e1a8c9e58e1b20dcbdf042',1,'pcg_detail::engine::advance(itype delta)'],['../classpcg__detail_1_1extended.html#ad917a2241597772e81668695c18ab2bb',1,'pcg_detail::extended::advance()']]]
+  ['addkillermove_0',['addKillerMove',['../classZagreus_1_1TranspositionTable.html#a7889afa9221436dab5c84475c11ac3ed',1,'Zagreus::TranspositionTable']]],
+  ['addoption_1',['addOption',['../classZagreus_1_1Engine.html#a88a7ddc125e98976af68011c08213a5a',1,'Zagreus::Engine']]],
+  ['addvar_2',['addVar',['../classZagreus_1_1UCIOption.html#a9bd71248e84bb86ff3dc177f5fba4c1a',1,'Zagreus::UCIOption']]],
+  ['advance_3',['advance',['../classpcg__detail_1_1engine.html#a12d3653cfe490a59f7ce55d028f1a7e0',1,'pcg_detail::engine::advance(itype state, itype delta, itype cur_mult, itype cur_plus)'],['../classpcg__detail_1_1engine.html#ad825cdb944e1a8c9e58e1b20dcbdf042',1,'pcg_detail::engine::advance(itype delta)'],['../classpcg__detail_1_1extended.html#ad917a2241597772e81668695c18ab2bb',1,'pcg_detail::extended::advance()']]]
 ];
