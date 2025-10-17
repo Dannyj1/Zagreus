7edce524c24255703425eca0427f8408dc0c7644
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2025-10-17 12:04:47 +0000

    Deploying to gh-pages from @ Dannyj1/Zagreus@275503a71f21da6b0b5ddf427860961ed3c96ab9 🚀

--- search/variables_5.js
+++ search/variables_5.js
@@ -8,8 +8,9 @@ var searchData=
   ['eg_5frook_5ftable_5',['eg_rook_table',['../namespaceZagreus.html#ab189515946f82df48dd4bd326e608c72',1,'Zagreus']]],
   ['endgamepsttable_6',['endgamePstTable',['../namespaceZagreus.html#a7b3cc214c8366a5b0a70e7743277d0a9',1,'Zagreus']]],
   ['enpassantsquare_7',['enPassantSquare',['../structZagreus_1_1BoardState.html#a8168f2482c44d4edf790701162e2a79f',1,'Zagreus::BoardState']]],
-  ['evalmaterialvalues_8',['evalMaterialValues',['../namespaceZagreus.html#a77212675e9ec1ea5afc27509b4afdd4a',1,'Zagreus']]],
-  ['evalmobility_9',['evalMobility',['../namespaceZagreus.html#a025a34fc4750a9af6e51d2c95b95c4d0',1,'Zagreus']]],
-  ['evalpassedpawnbonus_10',['evalPassedPawnBonus',['../namespaceZagreus.html#a3451e500915c199debf2ff0cf2dd701c',1,'Zagreus']]],
-  ['excludedmove_11',['excludedMove',['../structZagreus_1_1SearchStack.html#ae3898af71b7138f72996ff4a4d175170',1,'Zagreus::SearchStack']]]
+  ['evaldoubledpawnpenalty_8',['evalDoubledPawnPenalty',['../namespaceZagreus.html#a5f3c96f4d038028ca2915d6001233228',1,'Zagreus']]],
+  ['evalmaterialvalues_9',['evalMaterialValues',['../namespaceZagreus.html#a77212675e9ec1ea5afc27509b4afdd4a',1,'Zagreus']]],
+  ['evalmobility_10',['evalMobility',['../namespaceZagreus.html#a025a34fc4750a9af6e51d2c95b95c4d0',1,'Zagreus']]],
+  ['evalpassedpawnbonus_11',['evalPassedPawnBonus',['../namespaceZagreus.html#a3451e500915c199debf2ff0cf2dd701c',1,'Zagreus']]],
+  ['excludedmove_12',['excludedMove',['../structZagreus_1_1SearchStack.html#ae3898af71b7138f72996ff4a4d175170',1,'Zagreus::SearchStack']]]
 ];
