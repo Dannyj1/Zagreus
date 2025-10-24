b50faeee3dbae1ab3db977e6d8fd18d07175768e
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2025-10-24 09:46:00 +0000

    Deploying to gh-pages from @ Dannyj1/Zagreus@2998ee70c773ef458d365a29fec574b93c0aadef 🚀

--- search/variables_5.js
+++ search/variables_5.js
@@ -8,9 +8,11 @@ var searchData=
   ['eg_5frook_5ftable_5',['eg_rook_table',['../namespaceZagreus.html#ab189515946f82df48dd4bd326e608c72',1,'Zagreus']]],
   ['endgamepsttable_6',['endgamePstTable',['../namespaceZagreus.html#a7b3cc214c8366a5b0a70e7743277d0a9',1,'Zagreus']]],
   ['enpassantsquare_7',['enPassantSquare',['../structZagreus_1_1BoardState.html#a8168f2482c44d4edf790701162e2a79f',1,'Zagreus::BoardState']]],
-  ['evaldoubledpawnpenalty_8',['evalDoubledPawnPenalty',['../namespaceZagreus.html#a5f3c96f4d038028ca2915d6001233228',1,'Zagreus']]],
-  ['evalmaterialvalues_9',['evalMaterialValues',['../namespaceZagreus.html#a77212675e9ec1ea5afc27509b4afdd4a',1,'Zagreus']]],
-  ['evalmobility_10',['evalMobility',['../namespaceZagreus.html#a025a34fc4750a9af6e51d2c95b95c4d0',1,'Zagreus']]],
-  ['evalpassedpawnbonus_11',['evalPassedPawnBonus',['../namespaceZagreus.html#a3451e500915c199debf2ff0cf2dd701c',1,'Zagreus']]],
-  ['excludedmove_12',['excludedMove',['../structZagreus_1_1SearchStack.html#ae3898af71b7138f72996ff4a4d175170',1,'Zagreus::SearchStack']]]
+  ['evalbishopmobility_8',['evalBishopMobility',['../namespaceZagreus.html#ae180618cc8107d79ecad7b2413e52461',1,'Zagreus']]],
+  ['evaldoubledpawnpenalty_9',['evalDoubledPawnPenalty',['../namespaceZagreus.html#a5f3c96f4d038028ca2915d6001233228',1,'Zagreus']]],
+  ['evalknightmobility_10',['evalKnightMobility',['../namespaceZagreus.html#ad0ded8c6f600151fde7c1c0745fa4987',1,'Zagreus']]],
+  ['evalmaterialvalues_11',['evalMaterialValues',['../namespaceZagreus.html#a77212675e9ec1ea5afc27509b4afdd4a',1,'Zagreus']]],
+  ['evalqueenmobility_12',['evalQueenMobility',['../namespaceZagreus.html#adcf713ffac5827e45d0860ce840fc91c',1,'Zagreus']]],
+  ['evalrookmobility_13',['evalRookMobility',['../namespaceZagreus.html#adce0ba8129e6b5333715c575bbef1517',1,'Zagreus']]],
+  ['excludedmove_14',['excludedMove',['../structZagreus_1_1SearchStack.html#ae3898af71b7138f72996ff4a4d175170',1,'Zagreus::SearchStack']]]
 ];
