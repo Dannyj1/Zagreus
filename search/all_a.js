996f9b9071b9ec649ac3e3a09734f8232b169280
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2026-09-03 16:38:14 +0000

    Deploying to gh-pages from @ Dannyj1/Zagreus@8aba4348b99758875bfb478df82b10fb7f0e303c 🚀

--- search/all_a.js
+++ search/all_a.js
@@ -1,10 +1,7 @@
 var searchData=
 [
   ['king_0',['KING',['../namespaceZagreus.html#a6ff930e561c85660192d5277086e9577abbc1bdd5634088aa65b187af1b68b376',1,'Zagreus']]],
-  ['kingzone_1',['kingZone',['../structZagreus_1_1EvalData.html#a9a7d67c5ca7dad0e36b3ee334bbc93b9',1,'Zagreus::EvalData']]],
-  ['kingzoneattackercount_2',['kingZoneAttackerCount',['../structZagreus_1_1EvalData.html#a86600bbb692208c8a7076cba43a6f169',1,'Zagreus::EvalData']]],
-  ['kingzoneattacks_3',['kingZoneAttacks',['../structZagreus_1_1EvalData.html#a679fbfb7fc3dcf24ca878c949de26f05',1,'Zagreus::EvalData']]],
-  ['knight_4',['KNIGHT',['../namespaceZagreus.html#a6ff930e561c85660192d5277086e9577a44f109c25adab1281c20ef1c69c5901f',1,'Zagreus']]],
-  ['knight_5fpromotion_5',['KNIGHT_PROMOTION',['../namespaceZagreus.html#ae3d5464d72c8b3e99114db86d97e2caca7643de94376457be9c979a70bd0fa704',1,'Zagreus']]],
-  ['knightphase_6',['knightPhase',['../namespaceZagreus.html#ae2df05f6fadd8f4d561dc45821ddb549',1,'Zagreus']]]
+  ['knight_1',['KNIGHT',['../namespaceZagreus.html#a6ff930e561c85660192d5277086e9577a44f109c25adab1281c20ef1c69c5901f',1,'Zagreus']]],
+  ['knight_5fpromotion_2',['KNIGHT_PROMOTION',['../namespaceZagreus.html#ae3d5464d72c8b3e99114db86d97e2caca7643de94376457be9c979a70bd0fa704',1,'Zagreus']]],
+  ['knightphase_3',['knightPhase',['../namespaceZagreus.html#ae2df05f6fadd8f4d561dc45821ddb549',1,'Zagreus']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -1,7 +1,4 @@
 var searchData=
 [
-  ['kingzone_0',['kingZone',['../structZagreus_1_1EvalData.html#a9a7d67c5ca7dad0e36b3ee334bbc93b9',1,'Zagreus::EvalData']]],
-  ['kingzoneattackercount_1',['kingZoneAttackerCount',['../structZagreus_1_1EvalData.html#a86600bbb692208c8a7076cba43a6f169',1,'Zagreus::EvalData']]],
-  ['kingzoneattacks_2',['kingZoneAttacks',['../structZagreus_1_1EvalData.html#a679fbfb7fc3dcf24ca878c949de26f05',1,'Zagreus::EvalData']]],
-  ['knightphase_3',['knightPhase',['../namespaceZagreus.html#ae2df05f6fadd8f4d561dc45821ddb549',1,'Zagreus']]]
+  ['knightphase_0',['knightPhase',['../namespaceZagreus.html#ae2df05f6fadd8f4d561dc45821ddb549',1,'Zagreus']]]
 ];
