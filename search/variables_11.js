ad462ba8b0ab63bbb9d1a7b8c4f7465f87daabf0
Dannyj1 <73849717+Dannyj1@users.noreply.github.com>
2026-09-04 08:53:21 +0000

    Deploying to gh-pages from @ Dannyj1/Zagreus@877838ee84b54c9b6fcc5ccb9f1b471d38a1c753 🚀

--- search/variables_11.js
+++ search/variables_11.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['timespentms_0',['timeSpentMs',['../structZagreus_1_1SearchStats.html#a5cd01a93f513de2fcdad8b073038eb35',1,'Zagreus::SearchStats']]],
-  ['totalphase_1',['totalPhase',['../namespaceZagreus.html#a8ceb0b3cc0d026bfb9486b159cb10721',1,'Zagreus']]],
-  ['transpositiontable_2',['transpositionTable',['../classZagreus_1_1TranspositionTable.html#ab90f0aefbc8683bf17c8d6cb1b1143e6',1,'Zagreus::TranspositionTable']]]
+  ['tempo_0',['tempo',['../namespaceZagreus.html#a70dcb20fed70380b6416f9d0cde35d83',1,'Zagreus']]],
+  ['timespentms_1',['timeSpentMs',['../structZagreus_1_1SearchStats.html#a5cd01a93f513de2fcdad8b073038eb35',1,'Zagreus::SearchStats']]],
+  ['totalphase_2',['totalPhase',['../namespaceZagreus.html#a8ceb0b3cc0d026bfb9486b159cb10721',1,'Zagreus']]],
+  ['transpositiontable_3',['transpositionTable',['../classZagreus_1_1TranspositionTable.html#ab90f0aefbc8683bf17c8d6cb1b1143e6',1,'Zagreus::TranspositionTable']]]
 ];
